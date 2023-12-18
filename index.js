faafd4f8e5506c99f58aabd9eeed7bf389183899
halfcoke <halfcoke@163.com>
2023-12-18 16:44:25 +0800

    样式调整

--- index.js
+++ index.js
@@ -40,30 +40,81 @@ var chartDom = document.getElementById('chart-container');
 var myChart = echarts.init(chartDom);
 var option;
 
+let twoOption = {};
 
 $.get(
     DATA_PATH,
     function (data) {
+        function getLevelOption() {
+            return [
+                {
+                    itemStyle: {
+                        borderColor: '#c2c2c2',
+                        borderWidth: 0,
+                        gapWidth: 1
+                    },
+                    upperLabel: {
+                        show: false
+                    }
+                },
+                {
+                    itemStyle: {
+                        // borderColor: 'rgb(80,80,80)',
+                        borderColorSaturation: 0.8,
+                        borderWidth: 2,
+                        gapWidth: 1
+                    }
+                },
+                {
+                    colorSaturation: [0.6, 0.7],
+                    itemStyle: {
+                        borderWidth: 2,
+                        // borderColor: 'rgb(155,155,155)',
+                        gapWidth: 1,
+                        borderColorSaturation: 0.7
+                    }
+                },
+                {
+                    // colorSaturation: [0.35, 0.5],
+                    itemStyle: {
+                        borderWidth: 2,
+                        gapWidth: 1,
+                        borderColorSaturation: 0.6
+                    }
+                }
+            ];
+        }
         getValue(data)
         size(data)
         console.log(data)
         const treemapOption = {
+            leafDepth: 2,
             series: [
                 {
                     type: 'treemap',
                     id: 'echarts-package-size',
                     animationDurationUpdate: 1000,
-                    roam: false,
-                    nodeClick: undefined,
+                    roam: true,
+                    // nodeClick: undefined,
                     data: data.children,
                     universalTransition: true,
+                    visibleMin: 300,
                     label: {
                         show: true,
-                        overflow: 'truncate'
+                        overflow: 'break',
+                        align: 'center',
+                        padding: 1,
+                        fontsize: 10,
+                        position: 'inside'
                     },
                     breadcrumb: {
                         show: false
-                    }
+                    },
+                    upperLabel: {
+                        show: true,
+                        height: 30
+                    },
+                    levels: getLevelOption(),
                 }
             ]
         };
@@ -99,14 +150,28 @@ $.get(
                 }
             ]
         };
-        let currentOption = sunburstOption;
+
+        twoOption.treemapOption = treemapOption
+        twoOption.sunburstOption = sunburstOption
+        let currentOption = treemapOption;
         myChart.setOption(currentOption);
-        setInterval(function () {
-            currentOption =
-                currentOption === treemapOption ? sunburstOption : treemapOption;
-            myChart.setOption(currentOption);
-        }, 3000);
+        // setInterval(function () {
+        //     currentOption =
+        //         currentOption === treemapOption ? sunburstOption : treemapOption;
+        //     myChart.setOption(currentOption);
+        // }, 3000);
+        console.log(twoOption)
     }
 );
 
 option && myChart.setOption(option);
+let currentOption =  twoOption.treemapOption;
+$(document).ready(function (){
+    $(document).on("dblclick",function (event){
+        currentOption =
+                currentOption === twoOption.treemapOption ? twoOption.sunburstOption : twoOption.treemapOption;
+        myChart.setOption(currentOption);
+    })
+})
+
+
